1468e8f0c9aaa4fbf6f5de54be596112953ca0dc
Jonathan Santos <jon.si@icloud.com>
2026-01-06 14:49:37 -0300

    Upload da pasta Styles, com os arquivos global.css sendo o CSS global e o arquivo theme.css sendo as variáveis globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-// Um componente do React é uma função que utiliza o nome em PascalCase:
-// App
-// ExemploDeComponente
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
   return (
-    <div>
+    <>
       <h1>Olá mundo do App</h1>
       <p>meu primeiro componente</p>
-    </div>
+    </>
   );
 }
